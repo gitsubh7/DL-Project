56bc986c9a83fcd487c85036176d56be2a514489
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-03-31 13:40:21 +0530

    perspective transform module

--- BirdsEyeView_/perspective_transform/pytorch-two-GAN-master/test.py
+++ BirdsEyeView_/perspective_transform/pytorch-two-GAN-master/test.py
@@ -9,7 +9,7 @@ opt = TestOptions().parse()
 opt.nThreads = 1   # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
-opt.no_flip = True  # no flip
+opt.no_flip = True  
 
 data_loader = CreateDataLoader(opt)
 dataset = data_loader.load_data()
